098ca5caa57120e951115402967fac8f5c223c81
ciandre1976 <ciandre23@gmail.com>
2026-02-27 11:46:35 +0100

    simple animation

--- src/main.js
+++ src/main.js
@@ -10,13 +10,27 @@ const spriteHeight = 523;
 
 const playerImage = new Image();
 playerImage.src = "./shadow_dog.png";
-let x = 0;
+
+let frameX = 0;
+let frameY = 0;
+
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  //ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  /* ctx.drawImage(
-   
-  );*/
+  ctx.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+  );
+  if (frameX < 6) frameX++;
+  else frameX = 0;
 
   requestAnimationFrame(animate);
 }
